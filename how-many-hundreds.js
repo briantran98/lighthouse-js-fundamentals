c0ee21fbb8c5185d8d72ab06be353d4c25156fa7
Brian Tran <98brian.tran@gmail.com>
2020-05-06 03:42:42 +0000

    Added a couple of new files for functions and arrays

---
+++ how-many-hundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num) {
+  const remainder = num % 100;
+  const amount = (num - remainder) / 100;
+  return amount;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
